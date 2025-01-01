cc87fa09cc6bbcda86ff948f9e539660b3f920df
Hajime Fukuda <haji.fkd@gmail.com>
2025-01-01 11:36:04 +0000

    append scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,55 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: {
-        content_script: './src/content_script.ts',
-        background: './src/background.ts',
-        'options/options': './src/options/options.ts',
-        'popup/popup': './src/popup/popup.ts'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'cheap-module-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { context: 'src/' ,from: '**/*.html', to: '[path][name][ext]' },
-                { context: 'src/' ,from: '**/*.png', to: '[path][name][ext]' },
-                { context: 'src/' ,from: '**/*.json', to: '[path][name][ext]' }
-            ],
-        }),
-    ],
-    resolve: {
-        extensions: ['.ts', '.js']
-    }
+module.exports = (_env, argv) => {
+    const isProduction = argv.mode === 'production';
+    return {
+        entry: {
+            content_script: './src/content_script.ts',
+            background: './src/background.ts',
+            'options/options': './src/options/options.ts',
+            'popup/popup': './src/popup/popup.ts'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        devtool: 'cheap-module-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { context: 'src/', from: '**/*.html', to: '[path][name][ext]' },
+                    { context: 'src/', from: '**/*.png', to: '[path][name][ext]' },
+                    { context: 'src/', from: '**/*.json', to: '[path][name][ext]' }
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css'
+            })
+        ],
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        optimization: {
+            minimize: isProduction
+        }
+    };
 };
